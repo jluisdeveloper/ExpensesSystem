e2fe6df17d9e0635bc6cf582875b1b3fdf961975
NeoDante <jluis.developer@gmail.com>
2020-08-15 22:15:47 -0500

    modified scaffold entries for only responds to json

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -32,40 +32,23 @@ class EntriesController < ApplicationController
   # POST /entries.json
   def create
     @entry = Entry.new(entry_params)
+    @entry.save
 
-    respond_to do |format|
-      if @entry.save
-        format.html { redirect_to @entry, notice: 'Entry was successfully created.' }
-        format.json { render :show, status: :created, location: @entry }
-      else
-        format.html { render :new }
-        format.json { render json: @entry.errors, status: :unprocessable_entity }
-      end
-    end
+    render json: { params: params, notice: 'Entrada registrada exitosamente' }
   end
 
   # PATCH/PUT /entries/1
   # PATCH/PUT /entries/1.json
   def update
-    respond_to do |format|
-      if @entry.update(entry_params)
-        format.html { redirect_to @entry, notice: 'Entry was successfully updated.' }
-        format.json { render :show, status: :ok, location: @entry }
-      else
-        format.html { render :edit }
-        format.json { render json: @entry.errors, status: :unprocessable_entity }
-      end
-    end
+    @entry.update(entry_params)
+    render json: { notice: 'Entrada actualizada exitosamente' }
   end
 
   # DELETE /entries/1
   # DELETE /entries/1.json
   def destroy
     @entry.destroy
-    respond_to do |format|
-      format.html { redirect_to entries_url, notice: 'Entry was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render json: { notice: 'Entrada eliminada exitosamente' }
   end
 
   private
